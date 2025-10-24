fa21b8119656c2be42dba0b39e798e90cfbbf2c9
renan rabelo <renan.cesar.r@gmail.com>
2025-10-24 20:55:33 -0300

    postDeploy: substituir fetch/now por dependências HttpClient, Logger e Clock

--- src/services/postDeploy/types.ts
+++ src/services/postDeploy/types.ts
@@ -1,8 +1,11 @@
-export type FetchFn = typeof fetch;
+import { HttpClient } from '../../core/http/HttpClient.js';
+import { Logger } from '../../core/logging/Logger.js';
+import { Clock } from '../../core/time/Clock.js';
 
 export interface PostDeployDependencies {
-  fetch: FetchFn;
-  now: () => number;
+  http: HttpClient;
+  logger: Logger;
+  clock: Clock;
   setTimeout: (handler: () => void, timeout: number) => ReturnType<typeof setTimeout>;
   clearTimeout: (id: ReturnType<typeof setTimeout>) => void;
 }
