073d32e12c416149a2b9c6eb04475c93578b1ce3
renan rabelo <renan.cesar.r@gmail.com>
2025-10-24 20:56:09 -0300

    postDeploy: construir defaultPostDeployDependencies com HttpClient/Logger/Clock

--- src/services/postDeploy/dependencies.ts
+++ src/services/postDeploy/dependencies.ts
@@ -1,9 +1,12 @@
-import { performance } from 'node:perf_hooks';
+import { fetchHttpClient } from '../../utils/http.js';
+import { ConsoleLogger } from '../../core/logging/ConsoleLogger.js';
+import { SystemClock } from '../../core/time/SystemClock.js';
 import type { PostDeployDependencies } from './types.js';
 
 export const defaultPostDeployDependencies: PostDeployDependencies = {
-  fetch,
-  now: () => performance.now(),
+  http: fetchHttpClient,
+  logger: new ConsoleLogger(),
+  clock: new SystemClock(),
   setTimeout: (handler, timeout) => setTimeout(handler, timeout),
   clearTimeout: (id) => clearTimeout(id),
 };
