dd04abb452c1c6d1ce2474abeacf2ded70a6cd7c
renan rabelo <renan.cesar.r@gmail.com>
2025-10-24 21:20:41 -0300

    storage: montar default dependencies com HttpClient/StateRepository/Logger

--- src/services/storage/dependencies.ts
+++ src/services/storage/dependencies.ts
@@ -1,9 +1,13 @@
 import { azionApiBase, uploadConcurrency } from '../../utils/env.js';
-import { http } from '../../utils/http.js';
+import { fetchHttpClient } from '../../utils/http.js';
+import { fileStateRepository } from '../../utils/state.js';
+import { ConsoleLogger } from '../../core/logging/ConsoleLogger.js';
 import type { StorageDependencies } from './types.js';
 
 export const defaultStorageDependencies: StorageDependencies = {
   apiBase: azionApiBase(),
-  http,
+  http: fetchHttpClient,
+  state: fileStateRepository,
+  logger: new ConsoleLogger(),
   uploadConcurrency: uploadConcurrency,
 };
