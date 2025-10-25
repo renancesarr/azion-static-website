741dcda396819251b45612c2d3fbdd4738a7d58e
renan rabelo <renan.cesar.r@gmail.com>
2025-10-24 21:22:10 -0300

    storage: requer StateRepository nos helpers de lookup

--- src/services/storage/lookupBucket.ts
+++ src/services/storage/lookupBucket.ts
@@ -1,15 +1,15 @@
-import { readStateFile } from '../../utils/state.js';
 import { StorageBucketsState } from '../../models/storageBucketsState.js';
 import { StorageBucketRecord } from '../../models/storageBucketRecord.js';
 import { STORAGE_STATE_FILE } from './constants.js';
 import { normalizeStorageState } from './normalizeStorageState.js';
+import { StateRepository } from '../../core/state/StateRepository.js';
 
-export async function lookupBucketByName(name: string): Promise<StorageBucketRecord | undefined> {
-  const current = normalizeStorageState(await readStateFile<StorageBucketsState>(STORAGE_STATE_FILE));
+export async function lookupBucketByName(state: StateRepository, name: string): Promise<StorageBucketRecord | undefined> {
+  const current = normalizeStorageState(await state.read<StorageBucketsState>(STORAGE_STATE_FILE));
   return current.buckets[name];
 }
 
-export async function lookupBucketById(id: string): Promise<StorageBucketRecord | undefined> {
-  const current = normalizeStorageState(await readStateFile<StorageBucketsState>(STORAGE_STATE_FILE));
+export async function lookupBucketById(state: StateRepository, id: string): Promise<StorageBucketRecord | undefined> {
+  const current = normalizeStorageState(await state.read<StorageBucketsState>(STORAGE_STATE_FILE));
   return Object.values(current.buckets).find((bucket) => bucket.id === id);
 }
