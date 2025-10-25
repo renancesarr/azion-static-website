140e2ea3a1c2dcfab9dbfe9941357bbfaca32325
renan rabelo <renan.cesar.r@gmail.com>
2025-10-24 21:20:07 -0300

    storage: ampliar StorageDependencies com HttpClient/StateRepository/Logger

--- src/services/storage/types.ts
+++ src/services/storage/types.ts
@@ -1,13 +1,15 @@
-import type { HttpRequestOptions, HttpSuccess } from '../../utils/http.js';
 import { UploadCandidate } from '../../models/uploadCandidate.js';
 import { UploadIndexEntry } from '../../models/uploadIndexEntry.js';
 import { UploadReportEntry } from '../../models/uploadReportEntry.js';
-
-export type HttpFn = <T>(options: HttpRequestOptions) => Promise<HttpSuccess<T>>;
+import { HttpClient } from '../../core/http/HttpClient.js';
+import { StateRepository } from '../../core/state/StateRepository.js';
+import { Logger } from '../../core/logging/Logger.js';
 
 export interface StorageDependencies {
   apiBase: string;
-  http: HttpFn;
+  http: HttpClient;
+  state: StateRepository;
+  logger: Logger;
   uploadConcurrency: () => number;
 }
 
