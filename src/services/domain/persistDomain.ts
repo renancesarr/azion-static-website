9989c9b7b2d6967d0ad5b48d1660ddf35db7cb9d
renan rabelo <renan.cesar.r@gmail.com>
2025-10-26 20:16:22 -0300

    refactor(domain): persist with shared state contract

--- src/services/domain/persistDomain.ts
+++ src/services/domain/persistDomain.ts
@@ -1,6 +1,6 @@
 import { readStateFile, writeStateFile } from '../../utils/state.js';
 import { DomainRecord } from '../../models/entities/domainRecord.js';
-import type { DomainState } from '../../models/dto/domainState.js';
+import type { DomainState } from '../../models/shared/domainState.js';
 import { DOMAIN_STATE_FILE } from './constants.js';
 import { normalizeDomainState } from './normalizeDomainState.js';
 
