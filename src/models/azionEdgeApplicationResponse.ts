b7ed608de4b3617be1be3b91102ade76319e26d3
renan rabelo <renan.cesar.r@gmail.com>
2025-10-26 20:10:47 -0300

    refactor(models): remove edge application response root

--- src/models/azionEdgeApplicationResponse.ts
+++
@@ -1,6 +0,0 @@
-import { AzionEdgeApplication } from './azionEdgeApplication.js';
-
-export interface AzionEdgeApplicationResponse {
-  results?: AzionEdgeApplication;
-  data?: AzionEdgeApplication;
-}
