fc92950b5dba548b594562508e98592f99730598
renan rabelo <renan.cesar.r@gmail.com>
2025-10-26 20:10:56 -0300

    refactor(models): drop azion rule root dto

--- src/models/azionRule.ts
+++
@@ -1,9 +0,0 @@
-export interface AzionRule {
-  id: string;
-  phase: string;
-  order: number;
-  behaviors: unknown[];
-  criteria: unknown[];
-  created_at?: string;
-  [key: string]: unknown;
-}
