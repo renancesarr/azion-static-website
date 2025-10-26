6adbc709c817f7b636c0f7aa5ba4cf932adf115d
renan rabelo <renan.cesar.r@gmail.com>
2025-10-26 20:13:19 -0300

    refactor(models): add edge application response dto

---
+++ src/models/dto/azionEdgeApplicationResponse.ts
@@ -0,0 +1,6 @@
+import { AzionEdgeApplication } from './azionEdgeApplication.js';
+
+export interface AzionEdgeApplicationResponse {
+  results?: AzionEdgeApplication;
+  data?: AzionEdgeApplication;
+}
